8d1ec7ffaa52133b7ea7841081a106d1dcea9620
yys <1113728643@qq.com>
2021-02-17 23:41:59 +0800

    11111

--- shiro/src/test/java/cn/itcast/shiro/ShiroTest01.java
+++ shiro/src/test/java/cn/itcast/shiro/ShiroTest01.java
@@ -24,24 +24,7 @@ public class ShiroTest01 {
 
     @Test
     public void testLogin() {
-        //1.根据配置文件创建SecurityManagerFactory
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-test-1.ini");
-        //2.通过工厂获取SecurityManager
-        SecurityManager securityManager = factory.getInstance();
-        //3.将SecurityManager绑定到当前运行环境
-        SecurityUtils.setSecurityManager(securityManager);
-        //4.从当前运行环境中构造subject
-        Subject subject = SecurityUtils.getSubject();
-        //5.构造shiro登录的数据
-        String username = "zhangsan";
-        String password = "123456";
-        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
-        //6.主体登陆
-        subject.login(token);
-        //7.验证用户是否登录成功
-        System.out.println("用户是否登录成功="+subject.isAuthenticated());
-        //8.获取登录成功的数据
-        System.out.println(subject.getPrincipal());
+        System.out.println("杀死");
     }
 
 }
